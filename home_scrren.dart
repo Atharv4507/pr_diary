42f5634f5a908e4de91148ef5cd1ca9465957746
Atharv Hingmire <atharvmh2002@gmail.com>
2026-04-21 23:20:11 +0530

    fifth commit

--- home_scrren.dart
+++ home_scrren.dart
@@ -1,3 +1,21 @@
 class _MockHomeScrrenSubject extends Mock implements HomeScrrenSubject {
-  new MethodChannel('home_scrren_subject');
+  new MethodChannel('home_scrren_subject with network').setMockMethodCallHandler((call) async {
+    if (call.method == 'fetchData') {
+      return 'Mocked data';
+    }
+    return null;
+  });
 }
+
+BlocListener<HcBloc, HcState>(
+  listener: (context, state) {
+    HttpBlogListers.progressTracker(context, state = state.loading);
+    HttpBlogListers.handleError(context, state = state.error);
+  },
+  child:BlocBuilder<HomeCollectionBloc, HomeCollectionState>(
+  builder: (context, state) {
+    return Container();
+  },
+),
+)
+
--- main_screen.dart
+++ main_screen.dart
@@ -4,3 +4,14 @@ BlocBuilder<SubjectBloc, SubjectState>(
   },
 )
 const factory MainScreen.eventName() = _eventName;
+
+BlocConsumer<FcBloc, FcState>(
+  listener: (context, state) {
+    
+    HttpBlogListers.progressTracker(context, state = state.loading);
+    HttpBlogListers.handleError(context, state = state.error);
+  },
+  builder: (context, state) {
+    return Container();
+  },
+)
